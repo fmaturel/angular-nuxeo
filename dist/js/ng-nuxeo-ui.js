f278f6957e3283597619135f405b8bb41d8c483c
fmaturel <francoismaturel@gmail.com>
2015-09-24 01:58:28 +0200

    Adding possibility to fetch document by uid

--- dist/js/ng-nuxeo-ui.js
+++ dist/js/ng-nuxeo-ui.js
@@ -21,7 +21,7 @@ angular.module('ngNuxeoUI')
   }]);
 angular.module('ngNuxeoUI')
 
-  .directive('nuxeoDocument', ['$http', function () {
+  .directive('nuxeoDocument', ['nuxeo', function (nuxeo) {
 
     return {
       restrict: 'E',
@@ -29,12 +29,27 @@ angular.module('ngNuxeoUI')
       templateUrl: 'nuxeo-ui/views/nuxeo-document.html',
       scope: {
         entry: '=',
+        uid: '@',
         deletable: '@',
         publishTo: '@',
         onSuccess: '&',
         onError: '&'
       },
       link: function (scope, element, attr) {
+        if (!attr.entry) {
+          if (attr.uid) {
+            nuxeo.Document.query()
+              // Basic search
+              .withParentIn([attr.uid])
+              // Get data
+              .$get(function (data) {
+                scope.entry = data.entries.length && data.entries[0];
+              });
+          } else {
+            throw 'There should be either an entry or id in nuxeo document directive';
+          }
+        }
+
         if (attr.deletable === 'true') {
           scope.entry.isDeletable = true;
         }
